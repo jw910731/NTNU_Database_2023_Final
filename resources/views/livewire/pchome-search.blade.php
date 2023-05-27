6c41436952dec122159bdc0ed52089217e6e5312
KutsunaSubaRya <index20010928@gmail.com>
2023-05-28 01:06:44 +0800

    update: fix prod container

--- resources/views/livewire/pchome-search.blade.php
+++ resources/views/livewire/pchome-search.blade.php
@@ -1,4 +1,5 @@
 <div>
+    <div></div>
     <div class="mx-auto mt-8 w-2/3 p-4 bg-gray-300 overflow-hidden shadow-xl sm:rounded-lg">
         <form class="flex w-full" wire:submit.prevent="requestPCHome">
             <x-input wire:model.defer="search" class="w-11/12 h-8 p-4" />
@@ -6,26 +7,28 @@
             <x-button class="ml-4">Search</x-button>
         </form>
     </div>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="w-full">
-            @foreach ($result as $prod)
-                <div  class="flex flex-col bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+    <div class="w-4/5 py-12 mx-auto sm:px-6 lg:px-8">
+        @foreach ($result as $prod)
+            <div  class="bg-[#FEFCEC] flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row mx-8 sm:mx-8 md:mx-12 lg:m-x12 xl:mx-12 2xl:mx-12 mt-4">
+                <div class="flex flex-col justify-between p-4 leading-normal w-1/3 xl:w-1/5 2xl:w-1/5">
                     <a href="#">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="{{$prod['img']}}" alt="">
                     </a>
-                    <div class="flex flex-col justify-between p-4 leading-normal">
-                        <a href="">
-                            <h5 class="mt-4 text-2xl font-bold tracking-tight text-blue-600 hover:underline">{{$prod['name']}}</h5>
-                        </a>
-                        @if($prod['origin_price'] !== $prod['price'])
-                            <p class="mb-3 font-normal text-gray-700">原價: ${{$prod['origin_price']}}</p>
-                        @endif
-                        <p class="mb-3 font-normal text-gray-700">網路價: ${{$prod['price']}}</p>
-                    </div>
                 </div>
-            @endforeach
-        </div>
-        </div>
+                <div class="flex flex-col p-4 leading-normal w-1/3 xl:w-3/5 2xl:w-3/5">
+                    <a href="">
+                        <h5 class="mb-4 text-2xl font-bold tracking-tight text-blue-600 hover:underline">{{$prod['name']}}</h5>
+                    </a>
+                    <p class="mt-3 font-normal text-gray-700 overflow-hidden line-clamp-3">{{$prod['describe']}}</p>
+                </div>
+                <div class="flex flex-col items-center justify-between px-4 w-1/3 xl:w-1/5 2xl:w-1/5">
+                    @if($prod['origin_price'] !== $prod['price'])
+                        <p class="mb-3 font-normal text-xl text-gray-700">原價: ${{$prod['origin_price']}}</p>
+                    @endif
+                    <p class="mb-3 font-normal text-xl text-gray-700">網路價: ${{$prod['price']}}</p>
+                    <x-button class="mt-4 h-12 text-center rounded-lg md:rounded-lg md:rounded-lg">加入購物車</x-button>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
