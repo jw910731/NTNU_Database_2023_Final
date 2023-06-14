0ba63f50bb902e5d003bce8ba5a2b2c4c316680a
HsuJinho <hsujinho@gmail.com>
2023-06-14 15:34:19 +0800

    modify the view of payment

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -1,8 +1,32 @@
 @php
     $payment = \App\Models\Payment::all();
+    $cartItems = \App\Models\CartItem::all();
 @endphp
 <x-app-layout>
-    @livewire('cart-item-list', ['items'=> $cartItems])
+    @foreach($cartItems as $item)
+        @php
+            $prod = \App\Models\Product::where('Id', $item->product_id)->first();
+        @endphp
+        <div
+            class="bg-[#FEFCEC] flex flex-col items-center border border-gray-200 rounded-lg shadow md:flex-row mt-4 mx-8 sm:mx-8 md:mx-12 lg:m-x12 xl:mx-12 2xl:mx-12">
+            <div class="flex flex-col justify-between p-4 leading-normal w-1/3 xl:w-1/5 2xl:w-1/5">
+                <a href="{{route('product.show', ["product"=>$prod['pchome_id']])}}" target="_blank">
+                    <img
+                        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                        src="{{$prod['img']}}" alt="">
+                </a>
+            </div>
+            <div class="flex flex-col p-4 leading-normal w-1/3 xl:w-3/5 2xl:w-3/5">
+                <a href="{{route('product.show', ["product"=>$prod['pchome_id']])}}" target="_blank">
+                    <h5 class="mb-4 text-2xl font-bold tracking-tight text-blue-600 hover:underline">{{$prod['name']}}</h5>
+                </a>
+            </div>
+            <div class="flex flex-col items-center justify-between px-4 w-1/3 xl:w-1/5 2xl:w-1/5">
+                <p class="mb-3 font-normal text-xl text-gray-700">購買價格: $ {{$prod['price']}}</p>
+                <p class="mb-3 font-normal text-xl text-gray-700">購買數量: x {{$item['quantity']}}</p>
+            </div>
+        </div>
+    @endforeach
         <div class="flex flex-row-reverse mt-12 mb-4">
             <div class="mx-8 sm:mx-8 md:mx-12 lg:m-x12 xl:mx-12 2xl:mx-12 font-bold text-red-600">
                 @livewire('cart-total-money')
